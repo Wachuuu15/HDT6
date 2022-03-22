c83fcac50605ff9ac9ab5c771a94723a7b0993d4
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2022-03-21 18:24:47 -0600

    Producto

--- Principal.java
+++ Principal.java
@@ -7,14 +7,49 @@
 
 import java.io.File;
 import java.util.Scanner;
+import java.util.HashMap;
+
 
 public class Principal{
 	public static void main(String[] args) throws Exception{
-        File doc =
-          new File("C:\\Users\\jenni\\OneDrive\\Documentos\\GitHub\\HDT6\\ListadoProducto.txt");
-        Scanner obj = new Scanner(doc);
+		
+		Scanner sc = new Scanner(System.in);
+		boolean salida = false;
+		while(!salida) {
+			
+			System.out.println("Elija la opción que desea ejecutar ");
+			System.out.println("1. Mostrar lista de productos");//mostrar lista
+	        System.out.println("2. HashMap"); //Hashmap
+	        System.out.println("3. LinkedHashMap");
+	        int opc = sc.nextInt();
+	        sc.nextLine();
+	        
+	        
+	        switch(opc) {
+	        case 1: 
+	        	File doc =
+	        	new File("C:\\Users\\jenni\\OneDrive\\Documentos\\GitHub\\HDT6\\ListadoProducto.txt");
+	        	Scanner obj = new Scanner(doc);
+	        	
+	        	while (obj.hasNextLine())
+	              
+	        		System.out.println(obj.nextLine());
+	        	break;
+	        	
+	        case 2:
+	        	HashMap<String, Producto>productosmercado = new HashMap<String, Producto>();
+	        	productosmercado.put("Producto", new Producto("Bebida",1,"Chocolatito"));
+	        	
+	        	System.out.println(productosmercado);
+	        }
+		}
+		sc.close();
 
-        while (obj.hasNextLine())
-            System.out.println(obj.nextLine());
+		
         }
+	
+
+	
+	
+		
 }
---
+++ Producto.java
@@ -0,0 +1,89 @@
+/**
+ * 
+ * @author jenny
+ *
+ */
+
+
+public class Producto {
+
+	private String categoria;
+	private int cantidad;
+	private String descrip;
+	 
+	
+	public Producto(String categoria, int cantidad, String descrip) {
+		setCategoria(categoria);
+		setCantidad(cantidad);
+		setDescrip(descrip);
+	}
+	
+	
+
+	
+	
+	
+	public String getCategoria() {
+		return categoria;
+	}
+
+
+
+
+
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+
+
+
+
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+
+
+
+
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+
+
+
+
+
+	public String getDescrip() {
+		return descrip;
+	}
+
+
+
+
+
+
+	public void setDescrip(String descrip) {
+		this.descrip = descrip;
+	}
+
+
+
+
+
+
+	@Override
+	public String toString() {
+		return "Producto [categoria=" + categoria + ", cantidad=" + cantidad + ", descrip=" + descrip + "]";
+	}
+	
+	
+		
+		
+		
+}
